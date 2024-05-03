ace21d39a301ea790040db59c78a9ae4330c1548
wuubangdev <wuubangdev@gmail.com>
2024-05-03 14:25:30 +0700

    base quizdetail

--- src/components/User/QuizDetail.js
+++ src/components/User/QuizDetail.js
@@ -1,50 +1,75 @@
 import { useEffect, useState } from 'react';
 import { getQuizData } from '../../services/apiServices';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import _ from 'lodash';
-
+import './QuizDetail.scss';
 const QuizDetail = () => {
     const [detailQuiz, setDetailQuiz] = useState([])
-    let { id } = useParams();
+    const params = useParams()
+    const quizId = params.id;
     useEffect(() => {
         fetchQuestions();
-    }, [id])
-
+    }, [quizId])
+    const location = useLocation();
     const fetchQuestions = async () => {
-        let res = await getQuizData(id);
+        let res = await getQuizData(quizId);
         if (res && res.EC === 0) {
             let raw = res.DT;
-            console.log(raw)
-            let temp = _.chain(raw)
+            let data = _.chain(raw)
                 .groupBy("id")
-                .map((iteam) => {
-                    let description = '';
-                    let image = '';
+                .map((item) => {
                     let answers = [];
-                    iteam.forEach((value, index) => {
+                    let description = null;
+                    let image = null;
+                    let quizId = null;
+                    item.forEach((value, index) => {
                         if (index === 0) {
+                            quizId = value.id;
                             description = value.description;
                             image = value.image;
                         }
                         answers.push(value.answers);
                     })
                     return {
-                        quizId: iteam.id,
+                        quizId,
                         answers,
                         image,
                         description
                     }
                 })
                 .value()
-
-            console.log(temp)
-
+            console.log(data)
         }
     }
 
     return (
-        <div className='container'>
-            Quiz Details
+        <div className='container detail-quiz-container'>
+            <div className='left-content'>
+                <div className='title'>
+                    Quiz-{quizId}: {location?.state?.quizTitle}
+                </div>
+                <hr />
+                <div className='q-body'>
+                    <img src="" alt="" />
+                </div>
+                <div className='q-content'>
+                    <div className="question">
+                        Question 1:  How are you doing?
+                    </div>
+                    <div className="answer">
+                        <div className="answer-detail">A. asdas</div>
+                        <div className="answer-detail">B. qwdsadas</div>
+                        <div className="answer-detail">C. asdasdas</div>
+                    </div>
+                </div>
+                <div className='q-footer'>
+                    <button className='btn btn-primary'>Prev</button>
+                    <button className='btn btn-secondary'>Next</button>
+                </div>
+            </div>
+            <div className="right-content">
+                count down
+            </div>
         </div>
     )
 }
