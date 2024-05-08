5ca1f3d3ec10c01ce9f203dc90eec5a9f8a4fe6f
BangLV <wuubangdev@gmail.com>
2024-05-08 09:09:21 +0700

    old

---
+++ src/components/User/Question.js
@@ -0,0 +1,18 @@
+
+
+const Question = () => {
+    return (
+        <>
+            <div className="question">
+                Question 1:  How are you doing?
+            </div>
+            <div className="answer">
+                <div className="answer-detail">A. asdas</div>
+                <div className="answer-detail">B. qwdsadas</div>
+                <div className="answer-detail">C. asdasdas</div>
+            </div>
+        </>
+    )
+}
+
+export default Question;
